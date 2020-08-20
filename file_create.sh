95b14f9aec606c33a2f74cda0d8623852f0cd32b
Tharun-b777 <tharunkumaar7@gmail.com>
2020-08-20 08:22:46 +0530

    Typo

--- file_create.sh
+++ file_create.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo 'Name    Email    Roll' > file1.txt
-echo 'Roll    Branch' > file2.txt
-echo 'Email    Phone    GradYear' > file3.txt
+echo 'Branch    Roll' > file2.txt
+echo 'Email    Pho    GradYear' > file3.txt
